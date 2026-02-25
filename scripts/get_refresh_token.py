4c6a09c1aa0c5c43d9890a6e7d3b9a5a6463c917
galesandrei-jpg <gales.andrei@gmail.com>
2026-02-25 18:06:43 +0200

    Update get_refresh_token.py

--- scripts/get_refresh_token.py
+++ scripts/get_refresh_token.py
@@ -11,8 +11,11 @@ Docs note: monetary revenue requires yt-analytics-monetary.readonly scope.
 """
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ["https://www.googleapis.com/auth/yt-analytics-monetary.readonly"]
-
+SCOPES = [
+    "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
+    "https://www.googleapis.com/auth/yt-analytics.readonly",
+    "https://www.googleapis.com/auth/youtube.readonly",
+]
 def main():
     flow = InstalledAppFlow.from_client_secrets_file("oauth_client.json", SCOPES)
     creds = flow.run_local_server(port=8080, prompt="consent")
